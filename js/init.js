7101773d9e4115f8446298dfe1b16e263a7c0251
Michael Borde <michael.borde@ubimail.fr>
2016-10-28 12:37:03 +0200

    slides update

--- js/init.js
+++ js/init.js
@@ -1,30 +1,48 @@
 (function () {
   'use strict';
 
-  Reveal.initialize({
-    controls: false,
-    progress: true,
-    history: true,
-    center: true,
-    mouseWheel: true,
+  initializeReveal();
+  // handleMouseClick();
 
-    theme: Reveal.getQueryHash().theme || 'night',
-    transition: Reveal.getQueryHash().transition || 'none',
+  function initializeReveal() {
+    Reveal.initialize({
+      controls: false,
+      progress: true,
+      history: true,
+      center: true,
+      mouseWheel: true,
 
-    dependencies: [
-      {
-        src: 'lib/js/classList.js',
-        condition: function () {
-          return !document.body.classList;
+      theme: Reveal.getQueryHash().theme || 'beige',
+      transition: Reveal.getQueryHash().transition || 'none',
+
+      dependencies: [
+        {
+          src: 'lib/js/classList.js',
+          condition: function () {
+            return !document.body.classList;
+          }
+        },
+        {
+          src: 'reveal.js/plugin/highlight/highlight.js',
+          async: true,
+          callback: function () {
+            hljs.initHighlightingOnLoad();
+          }
         }
-      },
-      {
-        src: 'reveal.js/plugin/highlight/highlight.js',
-        async: true,
-        callback: function () {
-          hljs.initHighlightingOnLoad();
-        }
-      }
-    ]
-  });
+      ]
+    });
+  }
+
+  function handleMouseClick() {
+    window.addEventListener("mousedown", handleClick, false);
+    window.addEventListener("contextmenu", function (e) {
+      e.preventDefault();
+    }, false);
+
+    function handleClick(e) {
+      e.preventDefault();
+      if (e.button === 0) Reveal.next();
+      if (e.button === 2) Reveal.prev();
+    }
+  }
 })();
